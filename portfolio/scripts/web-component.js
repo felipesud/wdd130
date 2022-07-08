15f1fd008347566c18c2294b368fe210934ed562
Felipe Belisario <felipesantos.dsb@hotmail.com>
2022-07-07 22:22:04 -0300

    Add Timeline and Card

--- portfolio/scripts/web-component.js
+++ portfolio/scripts/web-component.js
@@ -1,4 +1,7 @@
 
+
+// How TO - Shrink Navigation Menu on Scroll
+
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
 window.onscroll = function() {scrollFunction()};
 
@@ -10,4 +13,35 @@ function scrollFunction() {
     document.getElementById("navbar").style.padding = "80px 10px";
     document.getElementById("logo").style.fontSize = "35px";
   }
+}
+
+// How TO - Slideshow Gallery
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 }
